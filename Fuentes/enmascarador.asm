e3e5ff3c17986b424f582555e2d208614afd0b74
Unknown <jony.alv.dev@outlook.com>
2018-11-18 07:38:58 -0300

    #9

--- Fuentes/enmascarador.asm
+++ Fuentes/enmascarador.asm
@@ -19,23 +19,51 @@ _enmascarar_asm:
 ;[EBP+12]   Puntero a img2
 ;[EBP+16]   Puntero a la mascara
 ;[EBP+20]   cantidad de bytes
-    MOV         EAX,    [EBP+8]    ;Desde C siempre tiene que venir múltiplo de 4? COnsultar con Carlos.
+    MOV         EAX,    [EBP+8]    ;Desde C siempre tiene que venir múltiplo de 4? Consultar con Carlos.
     MOV         EBX,    [EBP+12]
-    MOV         ECX,    [EBP+16]
-    XOR         EDX,    EDX
-    MOV         EDX,    [EBP+20]    
+    MOV         EDX,    [EBP+16]
+    XOR         ECX,    ECX
+    MOV         ECX,    [EBP+20]    
 CICLOENMASCARAR:
 ;Hay que controlar si vino vacío o si tamaño es menor a 4?
-    MOVUPS      XMM0,   [EAX]
-    MOVUPS      XMM1,   [EBX]
-    MOVUPS      XMM2,   [ECX]
+    CMP         ECX,     0
+    JE          FINALIZARCICLO
+    CMP         ECX,     15
+    JBE         ACOMODAR
+
+    MOVAPS      XMM0,   [EAX]
+    MOVAPS      XMM1,   [EBX]
+    MOVAPS      XMM2,   [EDX]
     ;LOGICA
-    ADD         EAX,    4
-    ADD         EBX,    4
-    ADD         ECX,    4
-    SUB         EDX,    4
-    CMP         EDX,    0
-    JNE         CICLOENMASCARAR
-;Fin de la función.            
+    PAND        XMM1,   XMM2       
+    PANDN       XMM2,   XMM2
+    PAND        XMM0,   XMM2    
+    PADDB       XMM0,   XMM1
+    
+    MOVAPS      [EAX],  XMM0
+    
+    ADD         EAX,    16
+    ADD         EBX,    16
+    ADD         EDX,    16
+    SUB         ECX,    16
+    
+    JMP         CICLOENMASCARAR
+    
+ACOMODAR:
+    ;mejorar
+    PUSH        EAX
+    MOV         EAX,    16
+    SUB         EAX,    ECX
+    MOV         ECX,    EAX
+    POP         EAX
+    SUB         EAX,    ECX
+    SUB         EBX,    ECX
+    SUB         EDX,    ECX
+    MOV         ECX,    16
+    JMP         CICLOENMASCARAR
+
+    ;CMP         EDX,    0
+    ;JNE         CICLOENMASCARAR
+FINALIZARCICLO:            
     POP         EBP
     RET
