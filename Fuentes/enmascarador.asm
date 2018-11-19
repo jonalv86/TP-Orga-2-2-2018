c88f83f5af9f713462ec23f219d3c3b11a6d7afa
Unknown <jony.alv.dev@outlook.com>
2018-11-19 11:43:45 -0300

    #7, #13, #14

--- Fuentes/enmascarador.asm
+++ Fuentes/enmascarador.asm
@@ -1,56 +1,67 @@
 ;Enter para testear=>%include "io.inc"
 section .data
 
+;Inicio Testing=>
+;img1: db 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128
+;img2: db 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
+;mask: db 0, 255, 255, 255, 0, 0, 255, 255, 255, 0, 0, 255, 255, 0, 0, 255, 255, 255, 0, 0, 0, 255, 255, 0, 0, 255 
+;cantidad: dd 26
+;<= Fin Testing
+
 global _enmascarar_asm
 
 section .text
-;Enter para testear=>global CMAIN
-;Enter para testear=>CMAIN:
-;Enter para testear=>    mov ebp, esp; for correct debugging
-    ;write your code here
-;Enter para testear=>    xor eax, eax
-;Enter para testear=>    ret
-
+;Inicio Testing=>
+;global CMAIN
+;CMAIN:
+;    mov ebp, esp; for correct debugging
+;    mov eax, img1
+;    mov ebx, img2
+;    mov ecx, mask
+;    ;xor edx, edx
+;    mov edx, [cantidad]
+;    push edx
+;    push ecx
+;    push ebx
+;    push eax
+;    call _enmascarar_asm
+;    add esp, 16
+;    xor eax, eax
+;    ret
+;<= Fin testing
 ;Inicio de la función.
 _enmascarar_asm:
     PUSH        EBP
     MOV	        EBP,    ESP
-;[EBP+8]    Puntero a img1
-;[EBP+12]   Puntero a img2
-;[EBP+16]   Puntero a la mascara
-;[EBP+20]   cantidad de bytes
-    MOV         EAX,    [EBP+8]    ;Desde C siempre tiene que venir múltiplo de 4? Consultar con Carlos.
-    MOV         EBX,    [EBP+12]
-    MOV         EDX,    [EBP+16]
+    MOV         EAX,    [EBP+8]     ;[EBP+8] Puntero a img1
+    MOV         EBX,    [EBP+12]    ;[EBP+12] Puntero a img2
+    MOV         EDX,    [EBP+16]    ;[EBP+16] Puntero a la mascara
     XOR         ECX,    ECX
-    MOV         ECX,    [EBP+20]    
+    MOV         ECX,    [EBP+20]    ;[EBP+20] Cantidad de bytes
+
 CICLOENMASCARAR:
-;Hay que controlar si vino vacío o si tamaño es menor a 4?
     CMP         ECX,     0
     JE          FINALIZARCICLO
     CMP         ECX,     15
     JBE         ACOMODAR
-
-    MOVAPS      XMM0,   [EAX]
-    MOVAPS      XMM1,   [EBX]
-    MOVAPS      XMM2,   [EDX]
+    MOVUPS      XMM0,   [EAX]
+    MOVUPS      XMM1,   [EBX]
+    MOVUPS      XMM2,   [EDX]
     ;LOGICA
     PAND        XMM1,   XMM2       
-    PANDN       XMM2,   XMM2
-    PAND        XMM0,   XMM2    
+    ;PANDN       XMM2,   XMM2   Esperaba que lo de vuelta. Tampoco hay una instruccion para negar la mascara. 
+    ;PAND        XMM0,   XMM2    
+    POR         XMM0,   XMM2
+    PXOR        XMM0,   XMM2
     PADDB       XMM0,   XMM1
-    
-    MOVAPS      [EAX],  XMM0
-    
+    MOVUPS      [EAX],  XMM0
     ADD         EAX,    16
     ADD         EBX,    16
     ADD         EDX,    16
     SUB         ECX,    16
-    
     JMP         CICLOENMASCARAR
     
 ACOMODAR:
-    ;mejorar
     PUSH        EAX
     MOV         EAX,    16
     SUB         EAX,    ECX
@@ -62,8 +73,7 @@ ACOMODAR:
     MOV         ECX,    16
     JMP         CICLOENMASCARAR
 
-    ;CMP         EDX,    0
-    ;JNE         CICLOENMASCARAR
-FINALIZARCICLO:            
+FINALIZARCICLO:
+    XOR         EAX,    EAX
     POP         EBP
     RET
--- Fuentes/enmascarador.c
+++ Fuentes/enmascarador.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define RGB 3
+#define lectura "rb"
+#define escritura "wb"
+#define ERROR 1
+#define OK 0
 
 int enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, long cant) {
 	int i=0;
@@ -9,74 +14,105 @@ int enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, long c
 			a[i] = b[i];
 		}
 	}
-	return 0;
+	return OK;
 }
 
-//extern 
-void enmascarar_asm(unsigned char *a, unsigned char *b, unsigned char *mask, int cant) { }
+extern int enmascarar_asm(unsigned char *a, unsigned char *b, unsigned char *mask, int cant);
 
-
-int leer_rgb(char *archivo, unsigned char *buffer, int ancho, int alto) {
-	FILE *archivoRead = NULL;
-	archivoRead = fopen(archivo, "rb");
-	fread(buffer, 1, ancho*alto*3, archivoRead);
+int leer_rgb(char *archivo, unsigned char *buffer, long tamanio) {
+	FILE *archivoRead = fopen(archivo, lectura);
+	long tamanioArchivo = fread(buffer, sizeof(unsigned char), tamanio, archivoRead);
 	fclose(archivoRead);
-	return 0;
+	return tamanioArchivo == tamanio ? OK : ERROR;
 }
 
-int escribir_rgb(char *archivo, unsigned char *buffer, int filas, int columnas) {
-	FILE *archivoWrite = NULL;
-	archivoWrite = fopen(archivo, "wb");
-	fwrite(buffer, 1, filas*columnas*3, archivoWrite);
+int escribir_rgb(const char *archivo, unsigned char *buffer, long tamanio) {
+	FILE *archivoWrite = archivoWrite = fopen(archivo, escritura);
+	long tamanioArchivo = fwrite(buffer, sizeof(unsigned char), tamanio, archivoWrite);
 	fclose(archivoWrite);
-	return 0;
+	return tamanioArchivo == tamanio ? OK : ERROR;
 }
-
 
 int main(int argc, char *argv[]) {
-	if(argc < 6) {
-		printf("Faltan par%cmetros.\n", 160);
-		return 1;
+	const int parametrosEsperados = 6;
+	const int aCuote = 160;
+	const int minAsm = 16;
+	const char *strFaltanParametros = "\nFaltan par%cmetros.\n";
+	const char *strTamanio = "%i x %i";
+	const char *strErrorDeLectura = "\nError en la lectura.\n";
+	const char *strErrorDeEscritura = "\nError en la escritura.\n";
+	const char *strErrorEnmascararC = "\nError al enmascarar en C.\n";
+	const char *strErrorEnmascararAsm = "\nError al enmascarar en Assembler.\n";
+	const char *strTamanioMinimo = ". El vector tiene menos de 16 bytes. No se puede operar optimamente en Assembler.\n";
+	const char *strTiempoTotal = ", %f segundos";
+	const char *nombreArchivoSalidaC = "salida_c.rgb";
+	const char *nombreArchivoSalidaAsm = "salida_asm.rgb";
+	
+	if(argc < parametrosEsperados) {
+		printf(strFaltanParametros, aCuote);
+		return ERROR;
 	}
+	
 	int ancho;
 	int alto;
-	long cantidad;
-	unsigned char *img1 = NULL;
-	unsigned char *img2 = NULL;
-	unsigned char *mascara = NULL;
-	
 	sscanf(argv[4], "%d", &ancho);
 	sscanf(argv[5], "%d", &alto);
-	cantidad = ancho * alto * 3;
+	printf(strTamanio, ancho, alto);
+	long cantidad = ancho * alto * RGB;
 	
-	img1 = malloc(cantidad);
-	img2 = malloc(cantidad);
-	mascara = malloc(cantidad);
+	int resultadoEjecucion = OK;	
+	unsigned char *img1 = malloc(cantidad);
+	unsigned char *img2 = malloc(cantidad);
+	unsigned char *mascara = malloc(cantidad);
 			
-	leer_rgb(argv[1], img1, ancho, alto);
-	leer_rgb(argv[2], img2, ancho, alto);
-	leer_rgb(argv[3], mascara, ancho, alto);
+	if (leer_rgb(argv[1], img1, cantidad) != OK ||
+		leer_rgb(argv[2], img2, cantidad) != OK ||
+		leer_rgb(argv[3], mascara, cantidad) != OK) {
+		printf(strErrorDeLectura);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
 	
 	clock_t inicio = clock();
-	enmascarar_c(img1, img2, mascara, cantidad);
-	clock_t fin = clock();
-	double tiempoTotal = (double)(fin - inicio) / CLOCKS_PER_SEC;
-	printf ("enmascarar_c demora %f segundos en una imagen de %i x %i\n", tiempoTotal, ancho, alto);
-	escribir_rgb("salida_c.rgb", img1, ancho, alto);	//img1 se encuentra pisado escribir archivo salida_c.rgb con el contenido de img1.
+	if (enmascarar_c(img1, img2, mascara, cantidad) != OK) {
+		printf(strErrorEnmascararC);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
+	printf (strTiempoTotal, (double)(clock() - inicio) / CLOCKS_PER_SEC);
+
+	if (escribir_rgb(nombreArchivoSalidaC, img1, cantidad) != OK) {
+		printf(strErrorDeEscritura);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
+	if (leer_rgb(argv[1], img1, cantidad) != OK) {
+		printf(strErrorDeLectura);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
 	
-	leer_rgb(argv[1], img1, ancho, alto);
-	//leer_rgb(argv[2], img2, ancho, alto);	//No estoy seguro si es necesario, hay que testear.
-	//TODO: Iniciar cronómetro. Ver si tiene mas de 16 bytes
+	if (cantidad < minAsm) {
+		printf(strTamanioMinimo);
+		goto FINALIZAR;
+	} 
 	inicio = clock();
-	enmascarar_asm(img1, img2, mascara, cantidad);
-	fin = clock();
-	tiempoTotal = (double)(fin - inicio) / CLOCKS_PER_SEC;
-	printf ("enmascarar_asm demora %f segundos en una imagen de %i x %i\n", tiempoTotal, ancho, alto);
-	escribir_rgb("salida_asm.rgb", img1, ancho, alto);	//img1 se encuentra pisado escribir archivo salida_asm.rgb con el contenido de img1.
+	if (enmascarar_asm(img1, img2, mascara, cantidad) != OK) {
+		printf(strErrorEnmascararAsm);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
+	printf (strTiempoTotal, (double)(clock() - inicio) / CLOCKS_PER_SEC);
 	
+	if (escribir_rgb(nombreArchivoSalidaAsm, img1, cantidad) != OK) {
+		printf(strErrorDeEscritura);
+		resultadoEjecucion = ERROR;
+		goto FINALIZAR;
+	}
+
+	FINALIZAR:
 	free(img1);
 	free(img2);
 	free(mascara);
-	
-	return 0;
+	return resultadoEjecucion;
 }
